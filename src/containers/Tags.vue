40b1ea8dbda7ab0dca796944b0c8bd2798d196f6
kmCha <chamajiuxi@126.com>
2016-11-08 21:07:51 +0800

    put tag in tags page

--- src/containers/Tags.vue
+++ src/containers/Tags.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="tags container" v-if="!loading">
-    <h1>敬请期待...</h1>
+    <div class="tag-wrapper">
+      <article-tag v-for="( tag, tagName ) in tags" :name="tagName"></article-tag>
+    </div>
   </div>
 </template>
 
 <script>
 import { loadingMixin } from '../mixins'
+import ArticleTag from '../components/ArticleTag'
 
 export default {
   data () {
@@ -14,16 +17,17 @@ export default {
     }
   },
   computed: {
-    count () {
-      return this.$store.getters.count
+    tags () {
+      return this.$store.state.tags
     }
   },
-  methods: {
-    inc () {
-      this.$store.commit('inc')
-    }
+  mounted () {
+
   },
-  mixins: [loadingMixin]
+  mixins: [loadingMixin],
+  components: {
+    ArticleTag
+  }
 }
 </script>
 
@@ -33,5 +37,19 @@ export default {
 
 .tags {
   text-align: center;
+}
+.tag-wrapper {
+  // position: absolute;
+  // top: 0;
+  // left: 0;
+  // right: 0;
+  // bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  div {
+    margin: 10px 0;
+  }
 }
 </style>
