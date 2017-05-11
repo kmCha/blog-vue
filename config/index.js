122f10fc21dbb48b584f49126878c639880dcc85
kmCha <chamajiuxi@126.com>
2017-05-11 10:34:20 +0800

    change dev port to 9998

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 9998,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
